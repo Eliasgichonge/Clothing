2acedec64112dcd2e8f768863746d80a4cba3035
Elias M. Mwita <eliasmwita86@gmail.com>
2025-04-04 04:38:31 +0300

    Register the Model in the Admin:

--- Clothes/shop/admin.py
+++ Clothes/shop/admin.py
@@ -1,7 +1,7 @@
-12.	from django.contrib import admin
-13.	from .models import Product
-14.
-15.	@admin.register(Product)
-16.	class ProductAdmin(admin.ModelAdmin):
-17.	    list_display = ('name', 'price', 'image')
-18.	    search_fields = ('name',)
+from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	    list_display = ('name', 'price', 'image')
+	    search_fields = ('name',)
