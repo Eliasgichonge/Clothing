00d74c06277c76912c5fa3972c6963bbdf837940
Elias M. Mwita <eliasmwita86@gmail.com>
2025-04-04 03:19:50 +0300

    Configure the Project

--- Clothes/Clothes/settings.py
+++ Clothes/Clothes/settings.py
@@ -128,4 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media',
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+# settings.py
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
