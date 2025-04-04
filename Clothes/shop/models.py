236ed54588042bd337c75a419298d85f52abf769
Elias M. Mwita <eliasmwita86@gmail.com>
2025-04-04 04:32:24 +0300

    Create the Product Model

--- Clothes/shop/models.py
+++ Clothes/shop/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
-# Create your models here.
+2.	from django.db import models
+3.
+4.	class Product(models.Model):
+5.	    name = models.CharField(max_length=100)
+6.	    price = models.DecimalField(max_digits=10, decimal_places=2)
+7.	    image = models.ImageField(upload_to='products/')
+8.
+9.	    def __str__(self):
+10.	        return self.name
