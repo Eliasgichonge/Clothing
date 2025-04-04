0158c3625631b7ed9ca707ca543214a293084e1a
Elias M. Mwita <eliasmwita86@gmail.com>
2025-04-04 04:30:01 +0300

    Configure URLs for Media:

--- Clothes/Clothes/urls.py
+++ Clothes/Clothes/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+19.	from django.conf import settings
+20.	from django.conf.urls.static import static
+21.	from django.contrib import admin
+22.	from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
