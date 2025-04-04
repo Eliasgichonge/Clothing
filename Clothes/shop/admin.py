879089456b63b142b947228ad64d4b7a2ac2ee41
Elias M. Mwita <eliasmwita86@gmail.com>
2025-04-04 03:47:24 +0300

    Mistake made in database

--- Clothes/shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-      list_display = ('name', 'price', 'image')
-      search_fields = ('name')
