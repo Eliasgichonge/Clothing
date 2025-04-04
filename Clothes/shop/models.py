4cde051ebf142c4ecafe64235f33d805bb574b0c
Elias M. Mwita <eliasmwita86@gmail.com>
2025-04-04 04:32:35 +0300

    Create the Product Model

--- Clothes/shop/models.py
+++ Clothes/shop/models.py
@@ -1,5 +1,6 @@
 2.	from django.db import models
-3.
+
+
 4.	class Product(models.Model):
 5.	    name = models.CharField(max_length=100)
 6.	    price = models.DecimalField(max_digits=10, decimal_places=2)
